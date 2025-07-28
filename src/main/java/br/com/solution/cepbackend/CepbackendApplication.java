6cf6a6c0d88e2545c269223c9f8b1f172a6f764e
Felipe <felipelagocoimbra@hotmail.com>
2025-07-27 22:11:05 -0300

    first commit

---
+++ src/main/java/br/com/solution/cepbackend/CepbackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.solution.cepbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/solution/cepbackend/CepbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solution.cepbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
